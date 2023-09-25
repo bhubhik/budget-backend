834ead5bb5c921c60e65566e39e50d6851178933
bhubhik <bhubhik@gmail.com>
2023-09-25 21:13:14 +1000

    income table

--- src/api/queries.js
+++ src/api/queries.js
@@ -7,4 +7,13 @@ CREATE TABLE public.expenses (
   )
 `;
 
+const createIncomeTable = `
+CREATE TABLE public.income (
+  id serial PRIMARY KEY,
+  description VARCHAR(255) NOT NULL,
+  type VARCHAR(255),
+  amount DECIMAL(10,2) NOT NULL
+);
+`;
+
 const getAllExpenses = 'SELECT * FROM expenses';
